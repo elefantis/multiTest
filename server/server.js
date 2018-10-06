d808ef37b44766016f57e490444cb97ea9a6e53d
Jean_Paul <jeanpft@infinitarium.net>
2018-10-06 19:24:54 -0300

    Conecta client y server con socket io

---
+++ server/server.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+const PORT = 3000;
+
+var messages = [{
+  id: 1,
+  text: "Hola soy un mensaje",
+  author: "Carlos Azaustre"
+}];
+
+app.use(express.static('public'));
+
+app.get('/hello', function(req, res) {
+  res.status(200).send("Hello World!");
+});
+
+io.on('connection', function(socket) {
+  console.log('Alguien se ha conectado con Sockets');
+  socket.emit('messages', messages);
+
+  socket.on('new-message', function(data) {
+    messages.push(data);
+
+    io.sockets.emit('messages', messages);
+  });
+});
+
+server.listen(PORT, function() {
+  console.log("Servidor corriendo en http://localhost:" + PORT);
+});
